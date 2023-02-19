6360c7477a9670ee5deaffed3037826019204a57
Marton Megyeri <megyerimarton@gmail.com>
2023-02-19 10:56:46 +0100

    Place random textures on top of the fields

--- src/components/Game/Board/Field/Field.tsx
+++ src/components/Game/Board/Field/Field.tsx
@@ -4,6 +4,7 @@ import styles from './Field.module.scss';
 import Flag from './Flag/Flag';
 import Mine from './Mine/Mine';
 import Text from './Text/Text';
+import Texture from './Texture/Texture';
 
 type Props = {
   text: string;
@@ -40,14 +41,15 @@ export default function Field({
       <motion.div
         style={{ y: -borderHeight }}
         whileTap={!isRevealed && !isFlagged ? { y: -borderHeight * 0.4 } : {}}
-        whileHover={{ y: -borderHeight * 0.75 }}
+        whileHover={!isRevealed && !isFlagged ? { y: -borderHeight * 0.75 } : {}}
         transition={{ type: 'spring', damping: 15, stiffness: 350 }}
         className={styles.content}
       >
         <AnimatePresence initial={false}>
-          {isRevealed && text && <Text>{text}</Text>}
-          {isRevealed && isMine && <Mine />}
-          {!isRevealed && isFlagged && <Flag />}
+          {isRevealed && text && <Text key={0}>{text}</Text>}
+          {isRevealed && isMine && <Mine key={1} />}
+          {!isRevealed && isFlagged && <Flag key={2} />}
+          {(!isRevealed || (isRevealed && !isMine)) && <Texture key={3} />}
         </AnimatePresence>
       </motion.div>
     </button>

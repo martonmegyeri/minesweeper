c5422ec713f07502e6d148ffa5be762f05bca406
Marton Megyeri <megyerimarton@gmail.com>
2023-02-27 20:53:29 +0100

    Redesign Settings and Toast, fix the side blur of PageTransition

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -3,8 +3,6 @@ import { shallow } from 'zustand/shallow';
 import { Theme, useSettings } from '../../stores/settings';
 import usePrevious from '../../utils/use-previous';
 import Modal from '../Modal/Modal';
-import { useToast } from '../Toast';
-import { ToastType } from '../Toast/Toasts';
 import Toggle from '../Toggle/Toggle';
 import styles from './Settings.module.scss';
 
@@ -15,42 +13,20 @@ export default function Settings() {
   );
   const [options, setOption] = useSettings(state => [state.options, state.setOption], shallow);
   const prevOptions = usePrevious(options);
-  const showToast = useToast(state => state.show);
 
   useEffect(() => {
     document.body.dataset.theme = options.theme;
   }, [options.theme]);
 
-  useEffect(() => {
-    if (prevOptions === undefined) return;
-
-    if (prevOptions?.theme !== options.theme) {
-      const on = options.theme === Theme.Dark;
-      const type = on ? ToastType.Success : ToastType.Info;
-      showToast(`Dark mode ${on ? 'on' : 'off'}`, type);
-    }
-
-    if (prevOptions?.parallaxMode !== options.parallaxMode) {
-      const on = options.parallaxMode;
-      const type = on ? ToastType.Success : ToastType.Info;
-      showToast(`Parallax Effect ${on ? 'on' : 'off'}`, type);
-    }
-  }, [options]);
-
   return (
     <Modal className={styles.settings} contentClassName={styles.modalBody} isOpen={isOpen} onClose={closeSettings}>
-      <div className={styles.block}>
-        <div className={styles.row}>
-          Dark Mode
-          <Toggle
-            on={options.theme === Theme.Dark}
-            onToggle={on => setOption('theme', on ? Theme.Dark : Theme.Light)}
-          />
-        </div>
-        <div className={styles.row}>
-          Parallax Effect
-          <Toggle on={options.parallaxMode} onToggle={on => setOption('parallaxMode', on)} />
-        </div>
+      <div className={styles.row}>
+        Dark Mode
+        <Toggle on={options.theme === Theme.Dark} onToggle={on => setOption('theme', on ? Theme.Dark : Theme.Light)} />
+      </div>
+      <div className={styles.row}>
+        Parallax Effect
+        <Toggle on={options.parallaxMode} onToggle={on => setOption('parallaxMode', on)} />
       </div>
     </Modal>
   );
--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -7,22 +7,28 @@ import styles from './Toggle.module.scss';
 type Props = HTMLProps<HTMLInputElement> & {
   on: boolean;
   onToggle: (on: boolean) => void;
+  pointTransformX?: number;
 };
 
-export default function Toggle({ on, onToggle, ...rest }: Props) {
+export default function Toggle({ on, onToggle, pointTransformX = 26, ...rest }: Props) {
   const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
     onToggle(event.target.checked);
   };
 
   return (
-    <div className={classNames(styles.toggle, { [styles.on]: on })}>
+    <div
+      className={classNames(styles.toggle, { [styles.on]: on })}
+      style={{ '--point-transform-x': `${pointTransformX}px` } as React.CSSProperties}
+    >
       <input {...rest} type="checkbox" checked={on} onChange={onChangeHandler} />
       <div className={styles.visual}>
         <motion.div
-          transition={{ type: 'spring', damping: 19, stiffness: 350 }}
-          animate={{ x: on ? 20 : 0 }}
+          transition={{ type: 'spring', damping: 20, stiffness: 300 }}
+          animate={{ x: on ? pointTransformX : 0 }}
           className={styles.dot}
-        ></motion.div>
+        >
+          {on ? 'on' : 'off'}
+        </motion.div>
       </div>
     </div>
   );

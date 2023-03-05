0a216b51c7be51886144869f6e5053b42ab6f6de
Marton Megyeri <megyerimarton@gmail.com>
2023-03-05 14:17:01 +0100

    Remove console.log

--- src/components/Game/Board/Board.tsx
+++ src/components/Game/Board/Board.tsx
@@ -37,8 +37,6 @@ export default function Board({ onStartTimer, onStopTimer, onMove, onPlaceFlag }
     }
   }, 0);
 
-  console.log(placedFlags);
-
   useEffect(() => {
     const filledBoard = getFilledBoard(selectedLevel);
     setBoard(filledBoard);
